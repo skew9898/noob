831aafd1ecb3b30ee27c98c554c883c0548755b3
skew9898 <103313128+skew9898@users.noreply.github.com>
2022-04-16 16:39:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title> Cloud Sound </title>
+	</head>
+	<body>
+		<form enctype="multipart/form-data" action="sound_action.php" method="POST">
+			<input type="hidden" name="MAX_FILE_SIZE"/>
+			Choose a file to upload: <input name="file" type="file" /><br />
+			<input type="submit" value="Upload File" />
+		</form>
+		<form enctype="multipart/form-data" action="sound_del_action.php" method="POST">
+			<input type="submit" value="Delete all Audio files" />
+		</form>
+		
+<?php
+$dir = "upload/";
+
+// Open a directory, and read its contents
+if (is_dir($dir)){
+  if ($dh = opendir($dir)){
+	 
+    while (($file = readdir($dh)) !== false){
+		$ext = pathinfo($file, PATHINFO_EXTENSION);
+		if(strcmp($ext,".mp3")){
+		echo "<p>filename:" . $file . "</p><br>";
+		echo "<audio controls>";
+		echo "<source src='upload/".$file."' type='audio/mp3'>";
+		echo "</audio>";
+		}
+    }
+    closedir($dh);
+  }
+}
+?>
+
+<footer>Copyright<small>&copy;</small>2018 - Yogesh Gupta</footer>
+	</body>
+</html>
+
